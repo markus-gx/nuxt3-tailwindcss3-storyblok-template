6d0b7ff958ec9a73426589f3f3400fd9851c5045
Markus Geilehner <markus@geilehner.io>
2022-01-24 15:54:49 +0100

    added settings; catchAllRoute

---
+++ src/pages/[...slug].vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="page">
+    <header>
+      <navigation></navigation>
+      <component v-for="blok in state.story.content.header" :key="blok._uid"
+                 v-editable="blok"
+                 :is="blok.component"
+                 v-bind="{...blok}"></component>
+    </header>
+    <main>
+      <component v-for="blok in state.story.content.body" :key="blok._uid"
+                 v-editable="blok"
+                 :is="blok.component"
+                 :blok="blok"
+      ></component>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, onMounted} from "@vue/runtime-core";
+import {useRoute, useRouter} from "vue-router";
+import {reactive} from "@vue/runtime-core";
+import useStoryApi from "~/composables/useStoryApi";
+import useStoryBridge from "~/composables/useStoryBridge";
+import CallToAction from "~/components/atoms/CallToAction.vue";
+import ImageStage from "~/components/atoms/ImageStage.vue";
+import useSettings from "~/composables/useSettings";
+import Navigation from "~/components/atoms/Navigation.vue";
+
+export default defineComponent({
+  components: {
+    Navigation,
+    CallToAction, ImageStage
+  },
+  async setup(){
+    const {nav} = useSettings()
+    const route = useRoute()
+    const router = useRouter()
+    const slugArray = route.params.slug as string[]
+    let slug = route.params.slug ? slugArray.join('/') : 'home'
+    const state = reactive({ story: undefined });
+    const storyapi = useStoryApi()
+
+    try{
+      const {data} = await storyapi.get(`cdn/stories/${slug}`, {version: 'draft'})
+      state.story = data.story
+      return {
+        state,
+        useStoryBridge,
+        nav
+      }
+    }catch(e){
+      router.push("/home")
+    }
+  },
+  mounted() {
+    useStoryBridge(this.state.story.id, (story) => (this.state.story = story))
+  }
+})
+</script>
