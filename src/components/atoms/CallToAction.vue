66baa147c6fe31a9d829b241a46a4b3446618dcb
Markus Geilehner <markus@geilehner.io>
2022-01-24 16:01:19 +0100

    Update CallToAction.vue

--- src/components/atoms/CallToAction.vue
+++ src/components/atoms/CallToAction.vue
@@ -1,7 +1,6 @@
 <template>
   <button
-      v-editable="blok"
-      class="py-2 px-8 text-black border-yellow-600 border uppercase tracking-wide text-xs transition-colors hover:bg-black hover:text-white hover:border-black">{{ blok.label }}</button>
+      class="py-2 px-8 text-black border-yellow-600 border uppercase tracking-wide text-xs transition-colors hover:bg-black hover:text-white hover:border-black">{{ label }}</button>
 </template>
 
 <script lang="ts">
@@ -10,8 +9,13 @@ import {defineComponent} from "@vue/runtime-core"
 export default defineComponent({
   name: "CallToAction",
   props: {
-    blok: {
-      type: Object
+    label: {
+      type: String,
+      default: ''
+    },
+    action: {
+      type: Object,
+      required: true
     }
   }
 })
